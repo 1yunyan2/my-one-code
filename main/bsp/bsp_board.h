94efa63107721b7b2ed27f5ecf5d9ffa9148abbc
skiinder <skiinder@live.cn>
2025-12-29 14:51:19 +0800

    feat(bsp): 添加LCD屏幕支持功能
    - 在CMakeLists.txt中添加bsp_board_lcd.c源文件
    - 在bsp_board.h中添加LCD相关头文件引用和LCD_BIT定义
    - 在bsp_board结构体中添加LCD相关的io和panel句柄
    - 新增bsp_board_lcd.c文件实现LCD初始化、开关屏功能
    - 在bsp_config.h中定义LCD相关的GPIO引脚配置
    - 实现LCD背光控制和显示开关功能

--- main/bsp/bsp_board.h
+++ main/bsp/bsp_board.h
@@ -6,12 +6,15 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/event_groups.h"
 #include "esp_codec_dev.h"
+#include "esp_lcd_panel_io.h"
+#include "esp_lcd_panel_dev.h"
 
 #define LED_BIT BIT0
 #define BUTTON_BIT BIT1
 #define WIFI_BIT BIT2
 #define NVS_BIT BIT3
 #define CODEC_BIT BIT4
+#define LCD_BIT BIT5
 
 typedef enum
 {
@@ -31,6 +34,10 @@ typedef struct
 
     // 音频设备
     esp_codec_dev_handle_t codec_dev;
+
+    // LCD
+    esp_lcd_panel_io_handle_t lcd_io;
+    esp_lcd_panel_handle_t lcd_panel;
 } bsp_board_t;
 
 bsp_board_t *bsp_board_get_instance(void);
@@ -48,6 +55,12 @@ void bsp_board_wifi_reset_provisioning(bsp_board_t *bsp_board);
 void bsp_board_nvs_init(bsp_board_t *bsp_board);
 
 void bsp_board_codec_init(bsp_board_t *bsp_board);
+
+void bsp_board_lcd_init(bsp_board_t *bsp_board);
+
+void bsp_board_lcd_on(bsp_board_t *bsp_board);
+
+void bsp_board_lcd_off(bsp_board_t *bsp_board);
 
 /* 检查特定标志位，如果所有检查的位都成功，返回true，否则返回false */
 bool bsp_board_check_status(bsp_board_t *bsp_board, EventBits_t bits_to_check, TickType_t wait_ticks);
